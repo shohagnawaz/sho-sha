18b977968fbc823efda8e13b7a7df427f4b5d04a
shohagnawaz <snawazdigital@gmail.com>
2020-11-05 00:54:23 +0600

    cart

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import './Product.css'
 
-const Product = () => {
+const Product = (props) => {
+
+    const { img, name, seller, price, stock } = props.product;
+
     return (
-        <div>
-            
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p><small>by: {seller}</small></p>
+                <p>${price}</p>
+                <p>Only {stock} left in stock - Order Soon</p>
+                <button onClick={ () => props.handlerAddProduct(props.product)} 
+                        className="main-button"><FontAwesomeIcon icon={ faCartPlus } /> 
+                        add to cart
+                </button>
+            </div>
         </div>
+
     );
 }
 
--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
 import './Shop.css';
 import fakeData from '../fakeData';
+import Product from './Product/Product';
 
 const Shop = () => {
 
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+    const handlerAddProduct = (product) => {
+        console.log("Product Added", product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
 
     return (
         <div className="shop-container">
             <div className="product-container">
-                <ul>
                     {
-                        products.map(product => <li>{product.name}</li>)
+                        products.map(pd => <Product handlerAddProduct={handlerAddProduct} product={pd}></Product>)
                     }
-                </ul>
             </div>
 
             <div className="cart-container">
                 <h1>This is cart</h1>
+                <h4>Order Summary: {cart.length}</h4>
             </div>
         </div>
     );
